19df21990cf12eeeff5337bff5a61ea0b4b10499
machado21 <grmachado12@gmail.com>
2018-01-28 18:34:59 -0400

    Layout cadastro da loja quase completo e funcionamento tambem
    Mas ainda falta cadastrar a loja no banco de dados

---
+++ app/src/main/java/com/prosoft/controledeestoque/Loja.java
@@ -0,0 +1,9 @@
+package com.prosoft.controledeestoque;
+
+/**
+ * Created by gabri on 28/01/2018.
+ */
+
+public class Loja {
+    //TODO
+}
--- app/src/main/java/com/prosoft/controledeestoque/Tela_Principal.java
+++ app/src/main/java/com/prosoft/controledeestoque/Tela_Principal.java
@@ -41,23 +41,21 @@ public class Tela_Principal extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela__principal);
-        Toolbar toolbar =  findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        /*TextView user_name = findViewById(R.id.user_name);
-        user_name.setText(user.getDisplayName());
-        TextView user_email = findViewById(R.id.email_text_view);
-        user_email.setText(user.getEmail());*/
         dialog = new Dialog(this);
-
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+            ImageView fechar;
+            Button btnCadLoja;
+
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Criar Nova Loja", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                mostrarDialogo(5);
             }
         });
 
@@ -67,21 +65,21 @@ public class Tela_Principal extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView =  findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
 
     }
 
     //Esse é para fazer com que a pessoa aperte duas vezes o botão para sair do aplicativo
-    Boolean DuasVezes=false;
+    Boolean DuasVezes = false;
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            if(DuasVezes){
+            if (DuasVezes) {
                 //Chamar a função para sair do aplicativo quando apertar duas vezes o botão de saída
                 action_sair();
             }
@@ -97,6 +95,7 @@ public class Tela_Principal extends AppCompatActivity
             }, 3000);
         }
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -121,8 +120,7 @@ public class Tela_Principal extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
-        else if (id == R.id.action_sair) {
+        } else if (id == R.id.action_sair) {
             //Função para sair do aplicativo
             action_sair();
         }
@@ -136,9 +134,9 @@ public class Tela_Principal extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_pag_inicial){
+        if (id == R.id.nav_pag_inicial) {
             Toast.makeText(this, R.string.função_indisponivel, Toast.LENGTH_SHORT).show();
-        }else if (id == R.id.nav_novo) {
+        } else if (id == R.id.nav_novo) {
             // Cadastrar novo produto
             mostrarDialogo(4);
         } else if (id == R.id.nav_gallery) {
@@ -152,10 +150,10 @@ public class Tela_Principal extends AppCompatActivity
         } else if (id == R.id.nav_reclamar) {
             //Número 2 - Reclamar
             mostrarDialogo(2);
-        } else if(id == R.id.nav_sair) {
+        } else if (id == R.id.nav_sair) {
             //Chamando a função para sair do aplicativo
             action_sair();
-        }else if(id == R.id.nav_sobre) {
+        } else if (id == R.id.nav_sobre) {
             //Número 3 - Sobre
             mostrarDialogo(3);
         }
@@ -164,8 +162,9 @@ public class Tela_Principal extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
     //Função para sair do aplicativo
-    public void action_sair(){
+    public void action_sair() {
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_HOME);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -173,15 +172,15 @@ public class Tela_Principal extends AppCompatActivity
         finish();
         System.exit(0);
     }
+
     //Função para mostrar falha de internet
-    public void mostrarDialogo(int i){
+    public void mostrarDialogo(int i) {
         ImageView fechar;
         Button btnA;
 
         if (i == 1) { //Tela de compartilhar
             Toast.makeText(this, R.string.função_indisponivel, Toast.LENGTH_SHORT).show();
-        }
-        else if (i == 2) { //Tela de reclamação
+        } else if (i == 2) { //Tela de reclamação
             final EditText recEm, recMsg;
 
             dialog.setContentView(R.layout.activity_reclamar);
@@ -201,16 +200,15 @@ public class Tela_Principal extends AppCompatActivity
                 public void onClick(View view) {
                     rec_em = recEm.getText().toString();
                     rec_msg = recMsg.getText().toString();
-                    if(rec_em.isEmpty() || rec_msg.isEmpty())
+                    if (rec_em.isEmpty() || rec_msg.isEmpty())
                         Toast.makeText(Tela_Principal.this, R.string.aviso_campo_vazio, Toast.LENGTH_SHORT).show();
-                    else{
+                    else {
                         Toast.makeText(Tela_Principal.this, R.string.reclamação_enviada, Toast.LENGTH_SHORT).show();
                         dialog.dismiss();
                     }
                 }
             });
-        }
-        else if (i == 3) {//Tela de Sobre
+        } else if (i == 3) {//Tela de Sobre
             dialog.setContentView(R.layout.activity_sobre);
             fechar = dialog.findViewById(R.id.img_close);
             btnA = dialog.findViewById(R.id.btn_ok);
@@ -227,8 +225,7 @@ public class Tela_Principal extends AppCompatActivity
                     dialog.dismiss();
                 }
             });
-        }
-        else if (i == 4) { //Tela de cadastro de novo produto
+        } else if (i == 4) { //Tela de cadastro de novo produto
             final EditText p_nome, p_des, p_qtd, p_valor, p_codigo;
 
             dialog.setContentView(R.layout.activity_cadastro_produto);
@@ -270,9 +267,9 @@ public class Tela_Principal extends AppCompatActivity
                         produto.setUsuario(user.getDisplayName()); //Busca a Informação do nome do usuario
                     }
 
-                    if(pdt_nome.isEmpty() || pdt_desc.isEmpty()|| pdt_qtd.isEmpty()|| pdt_valor.isEmpty()|| pdt_codigo.isEmpty())
+                    if (pdt_nome.isEmpty() || pdt_desc.isEmpty() || pdt_qtd.isEmpty() || pdt_valor.isEmpty() || pdt_codigo.isEmpty())
                         Toast.makeText(Tela_Principal.this, R.string.aviso_campo_vazio, Toast.LENGTH_SHORT).show();
-                    else{
+                    else {
                         //Enviando para o banco de dados no firebase versão beta
                         FirebaseDatabase database = FirebaseDatabase.getInstance();
                         DatabaseReference myRef = database.getReference("Importadora Manaus").child(produto.getDescricao()); // Salvnado de forma "correta" no banco de dados do firebase //TODO Arrumar onde esta "importadora manaus" colocar o nome da loja variavel
@@ -283,7 +280,50 @@ public class Tela_Principal extends AppCompatActivity
                     }
                 }
             });
+        } else if (i == 5) { // Tela de cadastro de Lojas
+            final EditText lNome, lCnpj, lProprietario, lSenha, lConfirmSenha;
+
+            dialog.setContentView(R.layout.activity_cadastro_loja);
+
+            lNome = dialog.findViewById(R.id.loja_nome);
+            lCnpj = dialog.findViewById(R.id.cnpj_ed);
+            lProprietario = dialog.findViewById(R.id.proprietario_ed);
+            lSenha = dialog.findViewById(R.id.senha_loja);
+            lConfirmSenha = dialog.findViewById(R.id.confirma_senha_loja);
+
+            btnA = dialog.findViewById(R.id.button_loja_cadastrar);
+            fechar = dialog.findViewById(R.id.button_fechar_loja);
+
+            fechar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                }
+            });
+
+            btnA.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if (lNome.getText().toString().isEmpty() || lCnpj.getText().toString().isEmpty() ||
+                            lProprietario.getText().toString().isEmpty() || lSenha.getText().toString().isEmpty() || lConfirmSenha.getText().toString().isEmpty())
+                        Toast.makeText(Tela_Principal.this, R.string.aviso_campo_vazio, Toast.LENGTH_SHORT).show();
+                    else {
+                        //TODO Enviar o objeto Loja que ainda falta finalizar para o banco de dados
+                        //Enviando para o banco de dados no firebase versão beta
+                        /*FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        * DatabaseReference myRef = database.getReference("Lojas").child(Loja.getDescricao()); // Salvnado de forma "correta" no banco de dados do firebase //TODO Arrumar onde esta "importadora manaus" colocar o nome da loja variavel
+                        * myRef.setValue(produto); // Envia toda a classe para o banco, sem problemas*/
+
+                        Toast.makeText(Tela_Principal.this, "Loja Cadastrada com Sucesso", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                }
+            });
+
         }
+        //...
+
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.show();
     }
